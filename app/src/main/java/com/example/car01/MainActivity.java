c6824e6bcf8f2e7de9f1f39071deb591ad4e4dd6
Lxxxx <1318753541@qq.com>
2021-04-22 17:12:06 +0800

    :2021年4月22日修改

--- app/src/main/java/com/example/car01/MainActivity.java
+++ app/src/main/java/com/example/car01/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.car01;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private Button button; //调用摄像头按钮
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        button = (Button) findViewById(R.id.main_button);
+        button.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), cameraActivity.class));
+            }
+        });
     }
 }
