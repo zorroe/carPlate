fcb827eea4ebbfc20379cff6d73cbee3d89ce606
‘lxxxx’ <1318753541@qq.com>
2021-05-28 16:57:59 +0800

    update V1.0

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 
-# 车牌识别客户端
+### 车牌识别客户端
 
 
-# 车牌识别服务端[点击](https://github.com/LiXuuuu/carPlateServer)
+### 车牌识别服务端 [点击](https://github.com/LiXuuuu/carPlateServer)
 
-
-
-# 项目计划书[点击](http://139.196.240.235:10000/schedule)
+### 项目计划书 [点击](http://139.196.240.235:10000/schedule)
 
 
 
-### 桌面端V0.1介绍
+### 桌面端V0.1
+
+#### 介绍
 
 主文件  [carPlateRec.py](https://github.com/LiXuuuu/carPlate/blob/0.1/carPlateRec.py)
 
@@ -31,7 +31,7 @@
    plate = getPlate(img_path)
    ```
 
-   * <font color=#00FF7F>**<u>解决方案</u>**</font>：优化后的方法直接将图像的base64编码解码后直接读取为pillow图像对象然后传入getPlate函数进行识别。节省IO操作耗费的时间
+   * <font color=#00FF7F>**<u>解决方案</u>**</font>：优化后的方法直接将图像的base64编码解码后直接读取为pillow图像对象然后传入getPlate函数进行识别。
 
    ```python
    img_data = base64.b64decode(request.POST.get('img'))
@@ -44,16 +44,46 @@
    * <font color=#FFA07A>**<u>原始</u>**</font>：所有函数都在一个线程，其中YoLo模型检测车牌，上传图片然后接受返回信息都是耗时操作。造成客户端卡顿且CPU占用高！
    * <font color=#00FF7F>**<u>解决方案</u>**</font>：将YoLo模型检测车牌和上传图像接受返回信息这两个操作放到子线程，每隔0.5s检测一帧图像，然后上传接收。
 
-![车牌](https://typora-lixuan.oss-cn-shanghai.aliyuncs.com/car.jpeg)
+<iframe 
+        src="//player.bilibili.com/player.html?aid=290828963&bvid=BV1yf4y1h7Zb&cid=345265126&page=1" 
+		width=800
+        height=600
+        scrolling="no" 
+        border="0" 
+        frameborder="no" 
+        framespacing="0" 
+        allowfullscreen="true"> 
+</iframe>
 
-图形化窗口：
+### 桌面端V1.0
 
-![图形化窗口](https://typora-lixuan.oss-cn-shanghai.aliyuncs.com/1.jpg)
+#### 介绍
 
-识别效果：
+- [x] 改变服务端处理接受图片的方式
+- [x] 将客户端的单线程版本改为多线程
 
-![识别效果](https://typora-lixuan.oss-cn-shanghai.aliyuncs.com/2.jpg)
+1.0版本将之前版本的两个问题进行了优化，第一个问题就是服务端处理接受图片的方式。具体提升效果如下表所示：
+
+|   获取图片方式   |   时间    |
+| :--------------: | :-------: |
+|   使用文件写读   | 3.8819384 |
+| 使用字节流直接读 | 3.3374874 |
+
+解决的第二个问题是单线程问题，V0.1版本的客户端为单线程版本，将耗时操作放在了主线程，然后V1.0版本将耗时操作放到子线程，界面流畅。
+
+<iframe 
+        src="//player.bilibili.com/player.html?aid=630790943&bvid=BV1db4y1o7gS&cid=345242156&page=1" 
+		height=600
+        width=800
+        scrolling="no" 
+        border="0" 
+        frameborder="no" 
+        framespacing="0" 
+        allowfullscreen="true"> 
+</iframe>
 
 ### release信息
 
 * 2021年5月23日：上传第一个0.1版本，本版本为单线程版本，界面卡顿，无法使用，后续会优化为多线程版本。
+
+* 2021年5月28日：上传第二个1.0正式版本，本版本为多线程版本，可以在不同配置的计算机上使用
--- carPlateRec.py
+++ carPlateRec.py
@@ -1,81 +1,117 @@
-import PySide2
-import cv2
-
 import base64
 import json
-import requests
-from PySide2.QtGui import QIcon
-from PySide2 import QtGui
-from PySide2.QtWidgets import QApplication
-from PySide2.QtUiTools import QUiLoader
-
-
 import os
+import cv2
+import time
+import numpy as np
+import PySide2
+import requests
+
+from threading import Thread
+from PySide2 import QtGui
+from PySide2.QtGui import QIcon
+from PySide2.QtUiTools import QUiLoader
+from PySide2.QtWidgets import QApplication
 from plateDetect import yolo
 
+# 初始化一个Yolo网络
 net = yolo()
 
+# 设置pyside2临时环境变量
 dirname = os.path.dirname(PySide2.__file__)
 plugin_path = os.path.join(dirname, 'plugins', 'platforms')
 os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
+
 
 class CarPlates:
     def __init__(self):
         # 从文件中加载ui
         # 从 UI 定义中动态 创建一个相应的窗口对象
-        # 注意：里面的控件对象也成为窗口对象的属性了
+        # 注意：里面的控件对象也成为窗口对象的属性
         self.ui = QUiLoader().load('carPlateRec.ui')
-        self.ui.start.clicked.connect(self.get_video)     #绑定开始识别
-        self.ui.end.clicked.connect(self.change_is_cap)   #绑定停止识别
+        self.ui.start.clicked.connect(self.get_video)
+        self.ui.end.clicked.connect(self.change_is_cap)
         self.ui.car.setText('摄像头未打开')
         self.is_cap = 1
         self.headers = {'content-type': 'application/x-www-form-urlencoded',
                         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36 Edg/90.0.818.41'}
         self.request_url = "http://139.196.240.235:10000/"
+        self.frame = ''
+        self.plates = []
+        self.cap_url = 'http://192.168.137.136:4747/video'
+        self.total_plate = ''
+        self.plate_str = ''
 
+    def get_plate(self):
+        self.frame, self.plates = net.return_frame(self.img)  # frame是标记后的图像，plates是单独的车牌图像列表
 
+    def show_str(self):
+        if len(self.plates) == 1:  # 110*35
+            self.total_plate = self.plates[0]
+            self.total_plate = cv2.resize(self.total_plate, dsize=(110, 35))
+            self.total_plate = cv2.cvtColor(self.total_plate, cv2.COLOR_BGR2RGB)
+            x = self.total_plate.shape[1]
+            y = self.total_plate.shape[0]
+            showImage = QtGui.QImage(self.total_plate.data, x, y, x * 3, QtGui.QImage.Format_RGB888)
+            self.ui.plate_img.setPixmap(QtGui.QPixmap.fromImage(showImage))
+        if len(self.plates) > 1:
+            self.total_plate = self.plates[0]
+            self.total_plate = cv2.resize(self.total_plate, dsize=(110, 35))
+            for i in range(1, len(self.plates)):
+                plate = self.plates[i]
+                plate = cv2.resize(plate, dsize=(110, 35))
+                self.total_plate = np.vstack([self.total_plate, plate])
+            self.total_plate = cv2.cvtColor(self.total_plate, cv2.COLOR_BGR2RGB)
+            x = self.total_plate.shape[1]
+            y = self.total_plate.shape[0]
+            showImage = QtGui.QImage(self.total_plate.data, x, y, x * 3, QtGui.QImage.Format_RGB888)
+            self.ui.plate_img.setPixmap(QtGui.QPixmap.fromImage(showImage))
 
+        self.plate_str = ''
+        for plate in self.plates:
+            image = cv2.imencode('.jpg', plate)[1]
+            base64_data = str(base64.b64encode(image))[2:-1]
+            params = {'img': base64_data}
+            response = requests.post(self.request_url, data=params, headers=self.headers)
+            # print(response.text)
+            if len(json.loads(response.text)['plate']) > 0:
+                # print(response.json())
+                self.plate_str += response.json()['plate']
+                self.plate_str += '\n'
+        self.ui.plate_char.setText(self.plate_str)
+        self.ui.plate_char.setStyleSheet("font-size:30")
 
-
-
-
- # 获取摄像头视频
+    # 获取摄像头视频
     def get_video(self):
         self.is_cap = 1
         self.cap = cv2.VideoCapture(0)
+        # self.cap = cv2.VideoCapture(self.cap_url)
+        t0 = time.clock()
         while (self.is_cap):
             ret, frame = self.cap.read()
-            frame, plates = net.return_frame(frame)
-            img = cv2.resize(frame, (640, 480))
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            showImage = QtGui.QImage(img.data, img.shape[1], img.shape[0], QtGui.QImage.Format_RGB888)
-            plate_str = ''
-            for plate in plates:
-                image = cv2.imencode('.jpg', plate)[1]
-                base64_data = str(base64.b64encode(image))[2:-1]
-                params = {'img': base64_data}
-                response = requests.post(self.request_url, data=params, headers=self.headers)
-                # print(response.text)
-                if len(json.loads(response.text)['plate']) > 0:
-                    # print(response.json())
-                    plate_str += response.json()['plate']       #plate_str是识别得到的车牌字符文本
-                    plate_str += '\n'
-            self.ui.plate_char.setText(plate_str)
-            self.ui.plate_char.setStyleSheet("font-size:30")    #将显示的车牌字符大小统一
+            self.img = frame
+            thread_get_plate = Thread(target=self.get_plate)
+            thread_show_str = Thread(target=self.show_str)
+            t1 = time.clock()
+            if t1 - t0 > 0.5:
+                thread_get_plate.start()
+                thread_show_str.start()
+                t0 = t1
+            self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
+            showImage = QtGui.QImage(self.img.data, self.img.shape[1], self.img.shape[0], QtGui.QImage.Format_RGB888)
             self.ui.car.setPixmap(QtGui.QPixmap.fromImage(showImage))
             QApplication.processEvents()
-            # time.sleep(0.04)
         self.cap.release()
         self.ui.car.setText('摄像头未打开')
 
-    def change_is_cap(self):   #设置为0
+    def change_is_cap(self):
         self.is_cap = 0
 
 
 
-app = QApplication([])
-# 加载 icon
-app.setWindowIcon(QIcon('logo.png'))
-carPlate = CarPlates()
-carPlate.ui.show()
-app.exec_()
+if __name__ == '__main__':
+    app = QApplication([])
+    app.setWindowIcon(QIcon('logo.png'))
+    carPlate = CarPlates()
+    carPlate.ui.show()
+    app.exec_()
